620427baa1dde29cd343fb1f6097865ea4cb5829
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-29 17:00:00 +0300

    changing the title and icon in the lower right part of the card

--- aggregator-front/src/components/special/SwiperElement.jsx
+++ aggregator-front/src/components/special/SwiperElement.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { BsCheckLg } from 'react-icons/bs' 
+import { AiOutlineRight } from 'react-icons/ai' 
 
 export default class SwiperElement extends Component {
     static propTypes = {
@@ -18,9 +18,9 @@ export default class SwiperElement extends Component {
                 </div>
                 <div className='absolute bottom-0 left-0 bg-gray-800 w-full text-left text-xl sm:text-[1.5rem] max-h-min text-primary-color py-7 px-4 sm:whitespace-nowrap rounded-lg overflow-hidden flex justify-between'>
                     {this.props.elementTitle}
-                    <button onClick={this.props.agreeHandler} className='choise-button invisible flex gap-2 justify-center items-center hover:text-green-400 duration-100 transition-colors cursor-pointer text-xl'>
-                        <p>Выбрать</p>
-                        <BsCheckLg/>
+                    <button onClick={this.props.agreeHandler} className='choise-button invisible flex justify-center items-center hover:text-green-400 duration-100 transition-colors cursor-pointer text-xl'>
+                        <p>Перейти</p>
+                        <AiOutlineRight/>
                     </button>
                 </div>
             </div>
