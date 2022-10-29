fd729f875faa491967c40daa56b528a1158f171d
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-29 09:45:01 +0300

    Restructuring

--- aggregator-front/src/components/ServiceSublist.jsx
+++ aggregator-front/src/components/ServiceSublist.jsx
@@ -1,19 +1,31 @@
-import axios from 'axios';
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { AiOutlineDown } from 'react-icons/ai'
+import Loader from './Loader';
 
 export default class ServiceSublist extends Component {
   static propTypes = {
     isOpen: PropTypes.bool,
-    chosenServiceId: PropTypes.number.isRequired
+    isLoading: PropTypes.bool.isRequired,
+    chosenServiceTitle: PropTypes.string,
+    subservices: PropTypes.array
   }
 
   static getDerivedStateFromProps(props, state) {
-    if (props.chosenServiceId !== state.chosenServiceId) {
+    if (props.chosenServiceTitle !== state.chosenServiceTitle) {
       return {
-        chosenServiceId: props.chosenServiceId
+        chosenServiceTitle: props.chosenServiceTitle
+      }
+    }
+    if (props.subservices !== state.subservices) {
+      return {
+        subservices: props.subservices
       };
+    }
+    if (props.isLoading !== state.isLoading) {
+      return {
+        isLoading: props.isLoading
+      }
     }
     return null;
   }
@@ -22,24 +34,22 @@ export default class ServiceSublist extends Component {
     super(props);
     this.state = {
       isOpen: false,
-      isLoading: false,
-      chosenServiceId: props.chosenServiceId
+      isLoading: props.isLoading,
+      chosenServiceTitle: props.chosenServiceTitle,
+      subservices: props.subservices
     };
 
     this.handleListOpen = this.handleListOpen.bind(this);
-  }
-
-  componentDidUpdate() {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1")
-      .then(response => {console.log(response.data)})
-      .catch(error => {console.log(error)});
   }
 
   handleListOpen() {
     this.setState(prevState => ({
       isOpen: !prevState.isOpen
     }));
-    console.log(this.state.isOpen)
+  }
+
+  componentDidUpdate() {
+    console.log(this.state)
   }
 
   render() {
@@ -47,11 +57,15 @@ export default class ServiceSublist extends Component {
       <div className='flex justify-center'>
         <div className={'bg-gray-700 shadow-xl shadow-gray-500 p-6 mb-10 sm:w-11/12 w-full transition-all duration-100 rounded-xl ' + (this.state.isOpen ? 'h-max' : 'h-20')}>
             { 
-              this.state.chosenServiceId === -1 ? <h1 className='text-gray-200 text-2xl'>Выберите услугу</h1> : (
-                <div className='text-white text-2xl border-b-2 border-b-gray-400 pb-2 flex justify-between items-center'>
-                  <h1>{this.state.chosenServiceId}</h1>
-                  <AiOutlineDown onClick={this.handleListOpen} className={'cursor-pointer hover:text-yellow-300 transition-all duration-100 text-3xl scale-x-[1.5] ' + (this.state.isOpen ? 'rotate-180' : 'rotate-0')}/>
-                </div>
+              this.state.subservices.length === 0 ? <h1 className='text-gray-200 text-2xl'>Выберите услугу</h1> : (
+                <>
+                {this.state.isLoading ? <Loader size={20} color="#ffffff"/> : (
+                  <div className='text-white text-2xl border-b-2 border-b-gray-400 pb-2 flex justify-between items-center'>
+                    <h1>{this.state.chosenServiceTitle}</h1>
+                    <AiOutlineDown onClick={this.handleListOpen} className={'cursor-pointer hover:text-yellow-300 transition-all duration-100 text-3xl scale-x-[1.5] ' + (this.state.isOpen ? 'rotate-180' : 'rotate-0')}/>
+                  </div>
+                )}
+                </>
               )
             }
         </div>
