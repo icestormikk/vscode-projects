2c6d660894555f147f46646921c67b1fa61bddb7
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-01 00:15:01 +0300

    new component for user notification

---
+++ aggregator-front/src/components/WarningPanel.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function WarningPanel({
+  colorCode, icon, header, content,
+}) {
+  return (
+    <div className={`flex flex-row items-center gap-5 rounded-xl p-2 bg-[${colorCode}] text-secondary-color w-full xl:w-1/2`}>
+      {icon}
+      <div className="flex flex-col">
+        <h1 className="text-xl font-bold">{header}</h1>
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+}
+
+WarningPanel.propTypes = {
+  colorCode: PropTypes.string.isRequired,
+  icon: PropTypes.objectOf(PropTypes.shape).isRequired,
+  header: PropTypes.string,
+  content: PropTypes.string,
+};
+
+WarningPanel.defaultProps = {
+  header: 'Предупреждение',
+  content: 'Что-то такое, о чём невозможно молчать',
+};
