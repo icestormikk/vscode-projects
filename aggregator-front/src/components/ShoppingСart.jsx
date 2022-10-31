8e69b5f84546bcebd3a3353508b87890fc72a13c
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-01 00:14:40 +0300

    smooth disappearance of the shopping basket when emptying and refilling

--- aggregator-front/src/components/ShoppingСart.jsx
+++ aggregator-front/src/components/ShoppingСart.jsx
@@ -7,7 +7,7 @@ export default function ShoppingСart() {
   const selectedSubservices = useSelector((state) => state.shoppingCart.selectedSubservices);
 
   return (
-    <div className={`fixed flex justify-center items-center ${selectedSubservices.length > 0 ? 'bottom-0' : '-bottom-20'} left-0 w-full p-4 bg-[#dddddd] text-2xl text-secondary-color`}>
+    <div className={`fixed flex justify-center items-center ${selectedSubservices.length > 0 ? 'bottom-0' : '-bottom-20'} left-0 w-full p-4 bg-[#dddddd] text-2xl text-secondary-color transition-all duration-200`}>
       <p>{selectedSubservices.length}</p>
     </div>
   );
