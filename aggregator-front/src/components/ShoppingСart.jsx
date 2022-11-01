41c85720a8b329aeb1a41c32d7bc0be6bb57299f
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-01 17:01:55 +0300

    increasing the interactivity of the component responsible for displaying information about selected services

--- aggregator-front/src/components/ShoppingСart.jsx
+++ aggregator-front/src/components/ShoppingСart.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable react/no-unused-class-component-methods */
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import Counter from './Counter';
+import { HiOutlineShoppingBag } from 'react-icons/hi';
+import Counter from './Counter';
 
 export default function ShoppingСart() {
   const selectedSubservices = useSelector((state) => state.shoppingCart.selectedSubservices);
 
   return (
-    <div className={`fixed flex justify-center items-center ${selectedSubservices.length > 0 ? 'bottom-0' : '-bottom-20'} left-0 w-full p-4 bg-[#dddddd] text-2xl text-secondary-color transition-all duration-200`}>
-      <p>{selectedSubservices.length}</p>
+    <div className={`fixed z-20 flex justify-center items-center ${selectedSubservices.length > 0 ? 'bottom-0' : '-bottom-20'} left-0 w-full p-4 bg-[#dddddd] text-2xl text-secondary-color transition-all duration-200`}>
+      <div className="flex gap-1 border-x-[1px] border-gray-500 px-4">
+        <HiOutlineShoppingBag className="text-3xl" />
+        <p>{selectedSubservices.length}</p>
+      </div>
+      <div className="border-x-[1px] border-gray-500 px-4">
+        <Counter
+          price={selectedSubservices.map(
+            (elem) => elem.topPrice,
+          ).reduce((partialSum, a) => partialSum + a, 0)}
+        />
+      </div>
     </div>
   );
 }
