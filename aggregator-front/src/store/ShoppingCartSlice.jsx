1e3468657af8e4622253582009a84f8060deb678
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-01 17:01:15 +0300

    improving the logic of the delete and add functions

--- aggregator-front/src/store/ShoppingCartSlice.jsx
+++ aggregator-front/src/store/ShoppingCartSlice.jsx
@@ -1,21 +1,43 @@
-/* eslint-disable no-console */
 import { createSlice } from '@reduxjs/toolkit';
 
 const shoppingCartSlice = createSlice({
   name: 'selectedSubservices',
   initialState: {
     selectedSubservices: [],
+    totalForbiddenSubservicesIDs: [],
   },
   reducers: {
     addSubserviceToCart(state, action) {
-      state.selectedSubservices.push(action.payload.subservice);
+      if (state.selectedSubservices.length < process.env.REACT_APP_SHOPPING_CART_LIMIT) {
+        const { subservice } = action.payload;
+
+        if (!state.totalForbiddenSubservicesIDs.includes(subservice.id)) {
+          state.selectedSubservices.push(subservice);
+
+          subservice.incompatibleServicesIDs.forEach(
+            (id) => {
+              if (!state.totalForbiddenSubservicesIDs.includes(id)) {
+                state.totalForbiddenSubservicesIDs.push(id);
+              }
+            },
+          );
+        }
+      }
     },
     removeSubserviceFromCart(state, action) {
+      const { subservice } = action.payload;
+      const subserviceId = subservice.id;
       const index = state.selectedSubservices.map(
         (elem) => elem.id,
-      ).indexOf(action.payload.subserviceId);
+      ).indexOf(subserviceId);
 
       if (index > -1) { state.selectedSubservices.splice(index, 1); }
+
+      action.payload.subservice.incompatibleServicesIDs.forEach((id) => {
+        if (!state.selectedSubservices.map((elem) => elem.id).includes(subservice.id)) {
+          state.totalForbiddenSubservicesIDs.pop(id);
+        }
+      });
     },
   },
 });
