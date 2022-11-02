ab9675fca6c356fa120d747f7073e404d28ca9bc
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-02 21:29:56 +0300

    refactoring

--- aggregator-front/src/components/special/SubserviceElement.jsx
+++ aggregator-front/src/components/special/SubserviceElement.jsx
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { BiLockAlt } from 'react-icons/bi';
 import { addSubserviceToCart } from '../../store/ShoppingCartSlice';
-import SubserviceCounter from './SubserviceCounter';
 import TimeDisplay from './TimeDisplay';
+import SubserviceChooser from './SubserviceChooser';
 
 export default function SubserviceElement({ subservice }) {
   const dispatch = useDispatch();
-  const allSubservices = useSelector((state) => state.shoppingCart.selectedSubservices);
+  const allSelectedSubservices = useSelector((state) => state.shoppingCart.selectedSubservices);
   const forbiddenServicesIDs = useSelector(
     (state) => state.shoppingCart.totalForbiddenSubservicesIDs,
   );
 
-  const currentSubserviceCount = allSubservices.filter((elem) => elem.id === subservice.id).length;
+  const isChosen = allSelectedSubservices.some((elem) => elem.id === subservice.id);
 
   return (
-    <div className="text-xl relative flex flex-col sm:flex-row justify-between items-center bg-gray-600 duration-100 p-2 rounded-lg h-max transition-all overflow-hidden">
+    <div className="text-xl relative flex flex-col sm:flex-row justify-between sm:items-center items-start bg-gray-600 duration-100 p-2 rounded-lg h-max transition-all overflow-hidden">
       <div className={`absolute top-0 left-0 w-full h-full text-xl text-gray-200 text-center bg-gray-700/80 ${forbiddenServicesIDs.includes(subservice.id) ? 'flex flex-col justify-center items-center' : 'hidden'}`}>
         <h1>Вы не можете выбрать данную услугу</h1>
         <BiLockAlt />
@@ -39,13 +39,15 @@ export default function SubserviceElement({ subservice }) {
         </div>
       </div>
       <div>
-        {currentSubserviceCount > 0
-          ? <SubserviceCounter subservice={subservice} count={currentSubserviceCount} />
+        {isChosen
+          ? <SubserviceChooser subservice={subservice} />
           : (
             <button
               type="button"
               className="bg-gray-500 hover:bg-green-500 transition-colors duration-100 py-2 px-4 rounded-lg"
-              onClick={() => { dispatch(addSubserviceToCart({ subservice })); }}
+              onClick={() => {
+                dispatch(addSubserviceToCart({ subservice }));
+              }}
             >
               Выбрать
             </button>
