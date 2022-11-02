7db4a69491e8cb568c12601a7f1ba3481208d6e6
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-02 21:27:10 +0300

    transferring the declaration of the root element of the modal window

--- aggregator-front/src/index.js
+++ aggregator-front/src/index.js
@@ -3,12 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import Modal from 'react-modal';
 import store from './store/store';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
