f98b9e9236ff45764238e5c7bbc19f8266bd6227
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-02 21:28:03 +0300

    refactoring + initialization of modal window

--- aggregator-front/src/components/ShoppingСart.jsx
+++ aggregator-front/src/components/ShoppingСart.jsx
@@ -1,9 +1,16 @@
-/* eslint-disable react/no-unused-class-component-methods */
+/* eslint-disable no-console */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
+import Modal from 'react-modal';
+import { AiOutlineClose } from 'react-icons/ai';
+import axios from 'axios';
 import Counter from './Counter';
 import TimeDisplay from './special/TimeDisplay';
+import { closeHeader } from '../store/HeaderSlice';
+import { hide, show } from '../store/ShoppingCartSlice';
+
+Modal.setAppElement('#root');
 
 function getRightWordForm(number) {
   if (number <= 20) {
@@ -17,8 +24,10 @@ function getRightWordForm(number) {
 }
 
 export default function ShoppingСart() {
+  const dispatch = useDispatch();
+  const isVisible = useSelector((state) => state.shoppingCart.isVisible);
   const selectedSubservices = useSelector((state) => state.shoppingCart.selectedSubservices);
-  const statisticsElementStyle = 'statsElement flex gap-1 sm:text-2xl text-base px-4 whitespace-nowrap';
+  const statisticsElementStyle = 'statsElement flex gap-1 sm:text-xl text-base px-4 whitespace-nowrap';
 
   const lowerTotalPrice = selectedSubservices.map(
     (elem) => elem.lowerPrice,
@@ -31,35 +40,74 @@ export default function ShoppingСart() {
     (elem) => elem.duration,
   ).reduce((partialSum, a) => partialSum + a, 0);
 
+  const [isModalOpen, setModalOpen] = React.useState(false);
+
   return (
-    <div className={`fixed z-20 flex justify-center items-center ${selectedSubservices.length > 0 ? 'bottom-0' : '-bottom-20'} left-0 w-full p-4 bg-[#dddddd] text-2xl text-secondary-color transition-all duration-200`}>
+    <div id="#shoppingcart" className={`fixed z-20 flex flex-col sm:flex-row justify-center items-center ${isVisible ? 'bottom-0' : '-bottom-full'} left-0 w-full p-4 bg-[#dddddd] text-xl text-secondary-color transition-all duration-200`}>
       <div className={statisticsElementStyle}>
-        <HiOutlineShoppingBag className="text-3xl" />
-        <p>{`${selectedSubservices.length} ${getRightWordForm(selectedSubservices.length)}`}</p>
+        <div className={statisticsElementStyle}>
+          <HiOutlineShoppingBag className="text-3xl" />
+          <p>{`${selectedSubservices.length} ${getRightWordForm(selectedSubservices.length)}`}</p>
+        </div>
+        <div className={statisticsElementStyle}>
+          <Counter
+            counterId="lowerPrice"
+            trackedValue={lowerTotalPrice}
+          />
+          {lowerTotalPrice !== topTotalPrice && (
+            <>
+              <p> - </p>
+              <Counter
+                counterId="topPrice"
+                trackedValue={topTotalPrice}
+              />
+            </>
+          )}
+          <p>RUB</p>
+        </div>
+        <div className={statisticsElementStyle}>
+          <TimeDisplay durationInMins={totalDuration} />
+        </div>
       </div>
       <div className={statisticsElementStyle}>
-        <Counter
-          counterId="lowerPrice"
-          trackedValue={lowerTotalPrice}
-        />
-        {lowerTotalPrice !== topTotalPrice && (
-          <>
-            <p> - </p>
-            <Counter
-              counterId="topPrice"
-              trackedValue={topTotalPrice}
-            />
-          </>
-        )}
-        <p>RUB</p>
-      </div>
-      <div className={statisticsElementStyle}>
-        <TimeDisplay durationInMins={totalDuration} />
-      </div>
-      <div className={statisticsElementStyle}>
-        <button type="button" className="text-xl text-gray-100 py-2 px-4 rounded-xl bg-gradient-to-r from-[#029872] to-[#09b68b]">
+        <button onClick={() => { setModalOpen(true); console.log(isModalOpen); }} type="button" className="text-xl text-gray-100 py-2 px-4 rounded-xl bg-gradient-to-r from-[#029872] to-[#09b68b] mt-2 sm:my-0">
           <p>Продолжить</p>
         </button>
+        <Modal
+          isOpen={isModalOpen}
+          onAfterOpen={() => {
+            dispatch(closeHeader());
+            dispatch(hide());
+          }}
+          onAfterClose={() => {
+            dispatch(show());
+          }}
+          onRequestClose={() => { setModalOpen(false); }}
+          contentLabel="Minimal Modal Example"
+          overlayClassName="Overlay"
+          className="Modal"
+          shouldCloseOnEsc
+          shouldCloseOnOverlayClick
+        >
+          <div>
+            <div className="flex w-full h-max justify-between items-center text-2xl text-black px-4 py-2 border-b-[1px] border-b-gray-400">
+              <p>Оформление заказа</p>
+              <AiOutlineClose onClick={() => { setModalOpen(false); }} />
+              <button
+                type="button"
+                onClick={() => {
+                  axios.get('/api/masters', {
+                    params:
+                      { services: selectedSubservices.map((elem) => elem.id) },
+                  });
+                }}
+              >
+                Send Request
+
+              </button>
+            </div>
+          </div>
+        </Modal>
       </div>
     </div>
   );
