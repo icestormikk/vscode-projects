e38dadff330a2aed99854e4ed9ad2afb703aa9c8
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-05 22:47:23 +0300

    Restructuring

--- aggregator-front/src/components/special/MastersChoise.jsx
+++ aggregator-front/src/components/special/MastersChoise.jsx
@@ -1,142 +1,148 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
 import Loader from '../Loader';
-import masterAvatar1 from '../../static/pictures/master_avatar_1.jpg';
-import masterAvatar2 from '../../static/pictures/master_avatar_2.jpg';
-import masterAvatar3 from '../../static/pictures/master_avatar_3.jpg';
+import { replaceMaster } from '../../store/OrdersInfoSlice';
 import getRandomCrewColor from '../../colorPallete';
+import ControllersButtonsPanel from './ControllersButtonsPanel';
+import LostComponent from './LostComponent';
 
-export default class MastersChoise extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: false,
-      selectedSubservices: props.selectedSubservices,
-      completeStageController: props.mastersCompletedController,
-      masters: [],
-    };
-  }
+Modal.setAppElement('#root');
 
-  componentDidMount() {
-    this.fetchMasters();
-  }
+export default function MastersChoise({
+  mastersCompletedController, selectedSubservices, subservicesToMasters, masters,
+}) {
+  const [isLoading, setLoading] = React.useState(true);
+  const [isModalOpen, setModalOpen] = React.useState(false);
+  const dispatch = useDispatch();
 
-  fetchMasters() {
-    const elementState = this.state;
-    this.setState(() => ({
-      isLoading: true,
-    }));
+  useState(() => {
+    setLoading(false);
+  });
 
-    axios.get('http://localhost:8080/date', {
-      params: {
-        services: elementState.selectedSubservices.map((subservice) => subservice.id),
-      },
-    })
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error))
-      .finally(() => {
-        this.setState(() => ({
-          isLoading: false,
-          masters: [
-            {
-              id: 0,
-              name: 'Имя',
-              surname: 'Фамилия',
-              professionName: 'Мастер по ноготочкам',
-              phone: '88005553535',
-              email: 'test1@test.com',
-              imageUrl: masterAvatar1,
-            }, {
-              id: 1,
-              name: 'Имя',
-              surname: 'Фамилия',
-              professionName: 'Профессиональный парикмахер',
-              phone: '88005553535',
-              email: 'test2@test.com',
-              imageUrl: masterAvatar2,
-            }, {
-              id: 2,
-              name: 'Имя',
-              surname: 'Фамилия',
-              professionName: 'Стажер-парикмахер',
-              phone: '88005553535',
-              email: 'test3@test.com',
-              imageUrl: masterAvatar3,
-            },
-          ],
-        }));
-      });
-  }
-
-  render() {
-    const pageState = this.state;
-    return (
-      pageState.isLoading ? (
-        <Loader
-          colorCode="#111827"
-          height={100}
-          width={100}
-          isOnFullScreen
-        />
-      ) : (
-        <div className="xl:w-1/2 lg:w-2/3 w-4/5">
-          <div className="flex justify-between items-center text-gray-500 ">
-            <h1 className="my-4 text-2xl">Выбор специалистов</h1>
-            <h1 className="my-4 text-xl whitespace-nowrap">1 / 3</h1>
-          </div>
-          <div className="flex flex-col gap-4">
-            {
-              pageState.masters && pageState.masters.map((master) => (
-                <div key={master.id} className="block rounded-xl bg-gray-200 border-[1px] border-gray-300">
-                  <div className="h-max py-4 px-4 border-[1px] border-gray-300 rounded-xl flex gap-4 bg-gray-100 relative overflow-hidden">
-                    <div className="flex gap-4 z-10 sm:flex-row flex-col">
-                      <img src={master.imageUrl} alt="master avatar" className="h-20 w-20 rounded-full border-[1px] border-gray-500 shadow-sm shadow-black" />
-                      <div className="break-all">
-                        <h1 className="text-base">
-                          {master.name}
-                          {' '}
-                          {master.surname}
-                        </h1>
-                        <h2 className="text-gray-500 italic font-extrabold" style={{ color: `${getRandomCrewColor()}` }}>
-                          {master.professionName}
-                        </h2>
-                      </div>
+  return (
+    isLoading ? (
+      <Loader
+        colorCode="#111827"
+        height={100}
+        width={100}
+        isOnFullScreen
+      />
+    ) : (
+      <>
+        <div className="flex justify-between items-center text-gray-500 ">
+          <h1 className="my-4 text-2xl">Выбор специалистов</h1>
+          <h1 className="my-4 text-xl whitespace-nowrap">1 / 3</h1>
+        </div>
+        <div className="flex flex-col gap-4">
+          {
+            Object.entries(subservicesToMasters).map((entry) => {
+              const [key, value] = entry;
+              if (value.length === 0) {
+                return (
+                  <LostComponent />
+                );
+              }
+              const currentSubservice = selectedSubservices.find((elem) => elem.id === parseInt(key, 10));
+              const selectedMasterId = value.find((elem) => elem.isSelected === true).id;
+              const selectedMaster = masters.find((elem) => elem.id === selectedMasterId);
+              return (
+                <div key={key} className="w-full bg-gray-200 border-[1px] border-gray-300 rounded-xl overflow-hidden block">
+                  <div className="flex flex-row bg-gray-100 p-4 gap-4 relative text-sm">
+                    <img src={selectedMaster.imageProfileUrl} alt="master's avatar" className="h-20 w-20 rounded-full border-[1px] border-gray-500 shadow-sm shadow-black" />
+                    <div className="flex flex-col gap-1">
+                      <h1 className="text-base">
+                        {selectedMaster.name}
+                        {' '}
+                        {selectedMaster.surname}
+                      </h1>
+                      <p className="font-light">{selectedMaster.professionTitle}</p>
                     </div>
+                    {
+                      value.length > 1 && (
+                        <>
+                          <button
+                            type="button"
+                            onClick={() => setModalOpen(true)}
+                            className="bg-gray-300 hover:bg-green-400 transition-colors duration-100 rounded-bl-xl p-2 absolute top-0 right-0 flex justify-center items-center gap-2"
+                          >
+                            <p>Выбрать</p>
+                            <FiMoreHorizontal className="text-xl" />
+                          </button>
+                          <Modal
+                            isOpen={isModalOpen}
+                            onRequestClose={() => setModalOpen(false)}
+                            shouldCloseOnOverlayClick
+                            shouldCloseOnEsc
+                            className="Modal"
+                            overlayClassName="Overlay"
+                          >
+                            <div className="flex justify-between items-center p-2">
+                              <p>Выберите специалиста</p>
+                              <button type="button" onClick={() => setModalOpen(false)}>Close</button>
+                            </div>
+                            <div>
+                              {
+                                value.map((val) => {
+                                  const currentMaster = masters.find((elem) => elem.id === val.id);
+                                  const subserviceId = key;
+                                  const newSelectedMasterId = val.id;
+                                  return !val.isSelected && (
+                                    <div key={val.id} className="flex flex-row bg-gray-100 p-4 gap-4 relative text-sm">
+                                      <img src={currentMaster.imageProfileUrl} alt="master's avatar" className="h-20 w-20 rounded-full border-[1px] border-gray-500 shadow-sm shadow-black" />
+                                      <div className="flex flex-col gap-1">
+                                        <h1 className="text-base">
+                                          {currentMaster.name}
+                                          {' '}
+                                          {currentMaster.surname}
+                                        </h1>
+                                        <p className="font-light">{currentMaster.professionTitle}</p>
+                                        <button
+                                          type="button"
+                                          className="bg-green-400 px-4 py-1 rounded-xl shadow-lg shadow-gray-400 hover:text-white transition-all duration-100 w-min"
+                                          onClick={() => {
+                                            dispatch(replaceMaster({ subserviceId, newSelectedMasterId }));
+                                            setModalOpen(false);
+                                          }}
+                                        >
+                                          Выбрать
+                                        </button>
+                                      </div>
+                                    </div>
+                                  );
+                                })
+                              }
+                            </div>
+                          </Modal>
+                        </>
+                      )
+                    }
                   </div>
-                  <div className="flex flex-col py-2 px-4">
-                    <h1>Название услуги 1</h1>
-                    <div className="flex gap-2 font-light">
-                      <p>2 ч 20 мин</p>
-                    </div>
+                  <div className="p-2">
+                    <p>{currentSubservice.title}</p>
                   </div>
                 </div>
-              ))
-            }
-          </div>
-          <div className="flex justify-between items-center my-4">
-            <Link to="/services">
-              <div className="bg-gradient-to-l from-[#e45353] to-[#e91f1f] p-2 text-base rounded-lg text-white">
-                Вернуться
-              </div>
-            </Link>
-            <button
-              type="button"
-              className="bg-gradient-to-r from-[#029872] to-[#09b68b] text-base p-2 rounded-lg text-white"
-              onClick={pageState.completeStageController}
-            >
-              Продолжить
-            </button>
-          </div>
+              );
+            })
+          }
         </div>
-      )
-    );
-  }
+        <ControllersButtonsPanel
+          addressToComeback="/services"
+          nextStageAction={mastersCompletedController}
+        />
+      </>
+    )
+  );
 }
 
 MastersChoise.propTypes = {
   mastersCompletedController: PropTypes.func.isRequired,
   selectedSubservices: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  subservicesToMasters: PropTypes.objectOf(PropTypes.shape).isRequired,
+  masters: PropTypes.arrayOf(PropTypes.shape).isRequired,
 };
--- aggregator-front/src/pages/OrderRegistration.jsx
+++ aggregator-front/src/pages/OrderRegistration.jsx
@@ -1,22 +1,110 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-console */
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import DateTimeChoise from '../components/special/DateTimeChoise';
 import MastersChoise from '../components/special/MastersChoise';
+import { addMaster, addMasterToSubservice } from '../store/OrdersInfoSlice';
+import masterAvatar1 from '../static/pictures/master_avatar_1.jpg';
+import masterAvatar2 from '../static/pictures/master_avatar_2.jpg';
+import masterAvatar3 from '../static/pictures/master_avatar_3.jpg';
 
 export default function OrderRegistration() {
-  const selectedSubservices = useSelector((state) => state.shoppingCart.selectedSubservices);
+  const dispatch = useDispatch();
+  const selectedSubservices = useSelector((state) => state.ordersInfo.selectedSubservices);
+  const subservicesToMasters = useSelector((state) => state.ordersInfo.subservicesToMasters);
+  const masters = useSelector((state) => state.ordersInfo.masters);
+
+  const [isReady, setReady] = React.useState(false);
   const [isMastersCompleted, setMastersCompleted] = React.useState(false);
+  const [isDateTimeCompleted, setDateTimeCompleted] = React.useState(false);
+
+  const testMasters = [{
+    id: 0,
+    name: 'Имя',
+    surname: 'Фамилия 0',
+    professionTitle: 'Мастер по ноготочкам',
+    phone: '88005553535',
+    email: 'test1@test.com',
+    imageProfileUrl: masterAvatar1,
+    providedServiceIDs: [0],
+  }, {
+    id: 1,
+    name: 'Имя',
+    surname: 'Фамилия 1',
+    professionTitle: 'Профессиональный парикмахер',
+    phone: '88005553535',
+    email: 'test2@test.com',
+    imageProfileUrl: masterAvatar2,
+    providedServiceIDs: [0],
+  }, {
+    id: 2,
+    name: 'Имя',
+    surname: 'Фамилия 2',
+    professionTitle: 'Стажер-парикмахер',
+    phone: '88005553535',
+    email: 'test3@test.com',
+    imageProfileUrl: masterAvatar3,
+    providedServiceIDs: [1, 2],
+  }];
+  const mastersForSubservices = [[0], [0], [1, 2]];
+
+  function updateState() {
+    setReady(false);
+    selectedSubservices.forEach((chosenSubservice) => {
+      axios.get('http://localhost:8080/masters', {
+        params: {
+          serviceId: chosenSubservice.id,
+        },
+      })
+        .then((response) => {
+          console.log('Response');
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log('Error');
+          console.log(error);
+        })
+        .finally(() => {
+          console.log('finally');
+          const chosenSubserviceId = chosenSubservice.id;
+
+          mastersForSubservices[chosenSubservice.id].forEach((masterId) => {
+            const master = testMasters.find((mst) => mst.id === masterId);
+            dispatch(addMaster({ master }));
+            dispatch(addMasterToSubservice({ chosenSubserviceId, masterId }));
+          });
+          setReady(true);
+        });
+    });
+    console.log(subservicesToMasters);
+  }
+
+  useEffect(() => {
+    updateState();
+  }, [selectedSubservices, subservicesToMasters]);
 
   return (
-    !isMastersCompleted ? (
-      <div className="min-h-screen text-black flex justify-center">
-        <MastersChoise
-          mastersCompletedController={setMastersCompleted}
-          selectedSubservices={selectedSubservices}
-        />
+    <div className="min-h-screen text-black flex justify-center">
+      <div className="xl:w-2/5 lg:w-1/2 w-4/5">
+        {isReady && (
+          !isMastersCompleted ? (
+            <MastersChoise
+              mastersCompletedController={setMastersCompleted}
+              selectedSubservices={selectedSubservices}
+              subservicesToMasters={subservicesToMasters}
+              masters={masters}
+            />
+          ) : (!isDateTimeCompleted ? (
+            <DateTimeChoise
+              dateTimeCompletedController={setDateTimeCompleted}
+            />
+          ) : (
+            <h1>DateTime section completed</h1>
+          ))
+        )}
       </div>
-    ) : (
-      <h1>Masters Stage completed</h1>
-    )
+    </div>
   );
 }
