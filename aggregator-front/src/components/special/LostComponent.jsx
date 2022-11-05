e9159ea678d4aaabc082649f29e0789e8555000f
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-05 22:48:39 +0300

    a component that can be called when any of the components failed to load

---
+++ aggregator-front/src/components/special/LostComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LostComponent() {
+  return (
+    <div className="w-full h-max p-2">
+      <p>Упс Не удалось загрузить компонент :</p>
+      <p>Попробуйте перезагрузить страницу</p>
+    </div>
+  );
+}
