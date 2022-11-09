1403e403941e12997f9bd92eca1abade8bbe2054
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-09 12:16:58 +0300

    functions for converting an array of dates into an object with multiple fields

---
+++ aggregator-front/src/datesTransformFunctions.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+export function toObject(datesArray) {
+  let index = 0;
+  return datesArray.map((elem) => ({
+    id: datesArray.length,
+    date: new Date(elem),
+    // eslint-disable-next-line no-plusplus
+    isSelected: index++ === 0,
+  }));
+}
+
+export function groupDatesObject(datesArray) {
+  const groupedDates = {};
+
+  datesArray.forEach((element) => {
+    const date = element.date.toDateString();
+    if (!groupedDates[date]) {
+      groupedDates[date] = {
+        timestamps: [],
+        isSelected: element.isSelected,
+      };
+    }
+    groupedDates[date].timestamps.push({
+      date: element.date,
+      isSelected: !groupedDates[date].timestamps.some((el) => el.isSelected)
+        && groupedDates[date].timestamps.length === 0
+        && Object.keys(groupedDates).length === 1,
+    });
+  });
+
+  return groupedDates;
+}
