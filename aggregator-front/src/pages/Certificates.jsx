4174440d2c695eee4356002e986bbd42af235353
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-15 16:02:15 +0300

    changing the appearance of certificates

--- aggregator-front/src/pages/Certificates.jsx
+++ aggregator-front/src/pages/Certificates.jsx
@@ -1,11 +1,11 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import { EffectCoverflow, Pagination } from 'swiper';
+import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Certificate from '../components/Certificate';
 import certificateImage1 from '../static/pictures/certificate-1000.jpg';
 import certificateImage2 from '../static/pictures/certificate-5000.jpg';
 import certificateImage3 from '../static/pictures/certificate-10000.jpg';
+import SpecialTag from '../components/SpecialTag';
 
 export default function Sertificates() {
   return (
@@ -21,24 +21,16 @@ export default function Sertificates() {
             <p>
               Срок действия сертификатов - 6 месяцев
             </p>
+            <h1 className="mt-20 text-lg">Мы предоставляем сертификаты номиналом 1 000, 5 000, 10 000 рублей</h1>
             <Swiper
               loop
+              navigation
               centeredSlidesBounds
-              effect="coverflow"
               id="swiper-color-dark"
               slidesPerView={1}
-              pagination={{
-                clickable: true,
-              }}
-              coverflowEffect={{
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows: true,
-              }}
-              modules={[EffectCoverflow, Pagination]}
-              className="max-w-[90rem] lg:w-[60vw] xl:w-p[40vw] sm:w-[80vw] w-[86vw] mt-28"
+              spaceBetween={30}
+              modules={[Navigation]}
+              className="max-w-[90rem] lg:w-[60vw] xl:w-p[40vw] sm:w-[80vw] w-[86vw] mb-2"
             >
               <SwiperSlide className="flex justify-center">
                 <Certificate
@@ -50,15 +42,28 @@ export default function Sertificates() {
                 <Certificate
                   imageUrl={certificateImage2}
                   nominalValue={5000}
+                  additionalTag={
+                    <SpecialTag text="Самое популярное!" colorCode="#de0e7a" />
+                  }
                 />
               </SwiperSlide>
               <SwiperSlide className="flex justify-center">
                 <Certificate
                   imageUrl={certificateImage3}
                   nominalValue={10000}
+                  additionalTag={
+                    <SpecialTag text="Самое выгодное!" colorCode="#e0bd2f" />
+                  }
                 />
               </SwiperSlide>
             </Swiper>
+            <button
+              type="button"
+              className="relative mb-10 w-max sm:px-20 px-4 py-2 sm:text-lg text-base text-white bg-green-400 rounded-xl h-max before:content-['']  before:absolute before:w-0 before:h-0 before:bottom-full before:left-1/2 before:-translate-x-1/2
+            before:border-[20px] before:border-transparent before:border-b-green-500 shadow-xl"
+            >
+              Приобрести
+            </button>
           </div>
         </div>
       </div>
