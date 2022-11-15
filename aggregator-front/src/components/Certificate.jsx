cb4707060497d6d45678637675d57ccbc75e5929
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-15 16:01:29 +0300

    changes in color

--- aggregator-front/src/components/Certificate.jsx
+++ aggregator-front/src/components/Certificate.jsx
@@ -5,16 +5,16 @@ export default function Certificate({
   imageUrl, nominalValue, additionalTag,
 }) {
   return (
-    <div className="relative w-full h-1/3 bg-yellow-100 rounded-sm flex flex-row overflow-hidden">
-      <div className="absolute top-10 left-10">
+    <div className="relative w-full h-1/3 bg-slate-100 rounded-xl flex flex-row overflow-hidden border-[1px] border-gray-300">
+      <div className="absolute top-0 left-0">
         {additionalTag}
       </div>
-      <img src={imageUrl} alt="certificate-icon" className="h-1/3 w-1/3" />
-      <div className="flex flex-col w-full justify-between text-center py-2 sm:px-24 px-2 font-light lg:text-2xl sm:text-base text-black">
-        <h1 className="text-base sm:text-xl">
+      <img src={imageUrl} alt="certificate-icon" className="h-1/3 w-1/3 shadow-xl shadow-gray-400" />
+      <div className="flex flex-col w-full justify-between text-center p-2 font-light lg:text-2xl sm:text-base text-black">
+        <h1 className="text-base sm:text-2xl">
           Подарочный сертификат
         </h1>
-        <p className="xl:text-[3.6rem] sm:text-[1.6rem] text-lg font-light italic whitespace-nowrap">
+        <p className="xl:text-[3.6rem] sm:text-[2.2rem] text-lg font-light italic whitespace-nowrap">
           {nominalValue.toLocaleString('ru')}
           {' '}
           рублей
