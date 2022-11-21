b488d184dd539e360f25a38e4bfe818dd5e553e0
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-21 18:29:05 +0300

    test changes on the "our works" page

--- aggregator-front/src/pages/Jobs.jsx
+++ aggregator-front/src/pages/Jobs.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import WorkInProgress from './WorkInProgress';
+import image1 from '../static/pictures/ourworks/img-1103.png';
+import image2 from '../static/pictures/ourworks/img-1104.png';
 
 export default function Jobs() {
-  return <WorkInProgress />;
+  return (
+    <div className="min-h-screen text-white flex justify-center">
+      <div className="w-11/12 px-4 my-20 h-full text-gray-500 text-xl flex justify-cnter">
+        <div className="w-full h-max bg-[#f5f3cc] flex justify-between rounded-sm overflow-hidden">
+          <img src={image1} alt="" className="object-contain" />
+          <img src={image2} alt="" className="object-fill" />
+        </div>
+      </div>
+    </div>
+  );
 }
