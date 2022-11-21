6a6f2fed520020854e98822f891f00cdb25e19fd
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-21 18:29:29 +0300

    restructuring

--- aggregator-front/src/pages/AdminPanel.jsx
+++ aggregator-front/src/pages/AdminPanel.jsx
@@ -8,7 +8,7 @@ export default function AdminPanel() {
     {
       id: 0,
       title: 'Все записи',
-      icon: <AiOutlineCalendar />,
+      icon: <AiOutlineCalendar className="text-3xl" />,
       element: <AllServiceEntries />,
     },
   ];
@@ -17,13 +17,13 @@ export default function AdminPanel() {
     <div className="min-h-screen text-white flex justify-center">
       <div className="w-11/12 px-4 mt-20 h-full text-gray-500">
         <h1 className="text-center text-xl pb-4">Добро пожаловать в панель администратора</h1>
-        <div className="flex flex-row w-full h-max border-[1px] border-gray-300 rounded-lg shadow-lg overflow-hidden">
+        <div className="flex flex-row w-full min-h-[20rem] border-[1px] border-gray-300 rounded-lg shadow-lg overflow-hidden">
           <div className="flex flex-col w-1/5 border-r-[1px] border-gray-300">
             {menuItems.map((item) => (
               <button
                 key={item.id}
                 type="button"
-                className="w-full px-2 py-1 hover:bg-[#075ca4] hover:text-white transition-all duration-100 text-secondary-color text-lg flex justify-start items-center gap-1"
+                className="w-full h-full px-2 py-1 hover:bg-[#075ca4] hover:text-white transition-all duration-100 text-secondary-color text-2xl flex justify-center items-center gap-1"
                 onClick={() => setChosenElement(item.element)}
               >
                 {item.icon}
@@ -31,13 +31,13 @@ export default function AdminPanel() {
               </button>
             ))}
           </div>
-          <div className="w-full h-full flex">
+          <div className="w-full h-full flex flex-col">
             {
               chosenElement === null
                 ? (
-                  <div className="w-full flex flex-col justify-center items-center gap-2 text-lg">
+                  <div className="w-full flex flex-col justify-center items-center gap-2 text-lg h-80">
                     <AiFillSetting className="text-[5rem]" />
-                    <h1>С большой силой приходит и большая ответственность..</h1>
+                    <h1 className="text-center">Текст</h1>
                   </div>
                 )
                 : chosenElement
