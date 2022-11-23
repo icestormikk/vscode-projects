67a77923639bb6b334645c33270fdf7678b12302
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-23 23:19:15 +0300

    minor changes in css styles

--- aggregator-front/src/pages/AdminPanel.jsx
+++ aggregator-front/src/pages/AdminPanel.jsx
@@ -15,9 +15,9 @@ export default function AdminPanel() {
 
   return (
     <div className="min-h-screen text-white flex justify-center">
-      <div className="w-11/12 px-4 mt-20 h-full text-gray-500">
+      <div className="w-11/12 px-4 mt-20 h-full text-gray-500 flex items-center flex-col">
         <h1 className="text-center text-xl pb-4">Добро пожаловать в панель администратора</h1>
-        <div className="flex flex-row w-full min-h-[20rem] border-[1px] border-gray-300 rounded-lg shadow-lg overflow-hidden">
+        <div className="flex flex-row w-11/12 min-h-[20rem] border-[1px] border-gray-300 rounded-lg shadow-lg overflow-hidden mb-4">
           <div className="flex flex-col w-1/5 border-r-[1px] border-gray-300">
             {menuItems.map((item) => (
               <button
