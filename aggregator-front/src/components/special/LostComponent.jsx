006f2a91eb72faf350b60900a43120cf34a0eaa1
icestormikk <jigalow.pavel2015@yandex.ru>
2022-11-28 18:58:01 +0300

    updated

--- aggregator-front/src/components/special/LostComponent.jsx
+++ aggregator-front/src/components/special/LostComponent.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BiError } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 export default function LostComponent() {
   return (
-    <div className="w-full h-max p-2">
-      <p>Упс.. Не удалось загрузить компонент :</p>
-      <p>Попробуйте перезагрузить страницу</p>
+    <div className="max-w-1/3 h-max p-2 text-gray-500 text-center italic text-xl flex justify-center items-center flex-col">
+      <BiError className="text-[6rem] text-red-500" />
+      <p>Отсутствуют данные для отображения..</p>
+      <span>
+        Попробуйте перезагрузить страницу или вернитесь на
+        {' '}
+        <Link to="/" className="underline font-bold">главную страницу</Link>
+      </span>
     </div>
   );
 }
