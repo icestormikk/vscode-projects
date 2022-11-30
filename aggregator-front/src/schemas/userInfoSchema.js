5cbc6002abf69d44812801122b87e3ad4eb0254d
icestormikk <jigalow.pavel2015@yandex.ru>
2022-12-01 00:03:55 +0300

    renaming and updating the validation scheme for user data

---
+++ aggregator-front/src/schemas/userInfoSchema.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/prefer-default-export */
+import * as Yup from 'yup';
+
+const requiredFieldMessage = 'Поле обязательно для заполнения';
+const PASSWORD_REGEX = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$';
+
+// Minimum eight, at least one uppercase letter,
+// one lowercase letter, one number and
+// one special character:
+
+export const userInfoRegistrationSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Имя администратора должно содержать не менее двух символов')
+    .required(requiredFieldMessage),
+  email: Yup.string()
+    .email('Укажите корректный адрес электронной почты')
+    .required(requiredFieldMessage),
+  phone: Yup.string()
+    .length(11, 'Некорректный формат номера телефона')
+    .required(requiredFieldMessage),
+  password: Yup.string()
+    .matches(PASSWORD_REGEX, 'Пароль не соответствует требованиям')
+    .required(requiredFieldMessage),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Пароли должны совпадать')
+    .required(requiredFieldMessage),
+});
+
+export const userInfoLoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Имя администратора должно содержать не менее двух символов')
+    .required(requiredFieldMessage),
+  password: Yup.string()
+    .matches(PASSWORD_REGEX, 'Пароль не соответствует требованиям')
+    .required(requiredFieldMessage),
+});
