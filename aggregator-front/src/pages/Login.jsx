ab09f7373931f63e44bbe5f35a0154595d2c8a16
icestormikk <jigalow.pavel2015@yandex.ru>
2022-12-01 00:02:23 +0300

    updating the login form

--- aggregator-front/src/pages/Login.jsx
+++ aggregator-front/src/pages/Login.jsx
@@ -1,17 +1,26 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable no-console */
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Logo from '../components/LogoComponent';
-import { adminInfoShema } from '../schemas/adminInfoSchema';
+import { userInfoLoginSchema } from '../schemas/userInfoSchema';
 
 export default function Login() {
+  // eslint-disable-next-line no-unused-vars
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const { message } = state || '';
+
   return (
     <div className="min-h-screen text-white flex justify-center">
       <div className="w-11/12 px-4 mt-20 h-full text-gray-500">
-        <h1 className="text-center p-2 text-xl">Вход в панель администратора</h1>
-        <div className="flex justify-center items-center">
-          <div className="border-[1px] border-gray-300 shadow-lg w-max h-max p-4 rounded-xl">
+        <div className="flex justify-center items-center flex-col gap-2">
+          {
+            message && (
+              <span className="text-xl text-red-600 text-center">{message}</span>
+            )
+          }
+          <div className="border-[1px] border-gray-300 shadow-lg w-max h-max p-4 rounded-xl ">
             <div className="p-10">
               <Logo
                 componentsAlignProperties="justify-center items-center"
@@ -24,21 +33,21 @@ export default function Login() {
                 username: '',
                 password: '',
               }}
-              validationSchema={adminInfoShema}
+              validationSchema={userInfoLoginSchema}
               onSubmit={(values) => console.log(values)}
             >
               {({ errors, touched }) => (
                 <Form className="flex flex-col gap-2">
                   <label htmlFor="username" className="flex flex-col">
-                    <span>Имя администратора</span>
-                    <Field name="username" id="username" className="input-field-special-style" />
+                    <span>E-mail</span>
+                    <Field name="username" id="username" className="input-field-special-style w-full" />
                     {touched.username && errors.username ? (
                       <p className="error-label">{errors.username}</p>
                     ) : null}
                   </label>
                   <label htmlFor="password">
                     <p>Пароль</p>
-                    <Field name="password" id="password" className="input-field-special-style" />
+                    <Field name="password" id="password" className="input-field-special-style w-full" />
                     {touched.password && errors.password ? (
                       <p className="error-label">{errors.password}</p>
                     ) : null}
@@ -51,6 +60,10 @@ export default function Login() {
                 </Form>
               )}
             </Formik>
+            <div className="flex gap-2 justify-center items-center mt-4">
+              <p>Нет аккаунта?</p>
+              <Link to="/registration" className="underline">Зарегистрироваться</Link>
+            </div>
           </div>
         </div>
       </div>
