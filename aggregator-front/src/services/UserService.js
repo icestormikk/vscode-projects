c46afa74754c20314f5950766b09bdbfa3be7d76
icestormikk <jigalow.pavel2015@yandex.ru>
2022-12-01 19:25:17 +0300

    adding methods and default values for the User entity

--- aggregator-front/src/services/UserService.js
+++ aggregator-front/src/services/UserService.js
@@ -1,11 +1,36 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-const usersPath = 'http://localhost:8080/auth';
+const usersPath = 'http://localhost:8080/users';
+
+export const defaultUser = {
+  id: 0,
+  firstname: 'Vasya',
+  lastname: 'Pupkin',
+  phone: '88005553535',
+  email: 'vasyapupkin@test.com',
+  roles: ['user'],
+};
 
 export const UsersAPI = {
   registerUser(user) {
     return axios
       .post(usersPath, user);
+  },
+  updateUser(user) {
+    return axios
+      .patch(usersPath, user);
+  },
+  deleteUser(user) {
+    return axios
+      .delete(usersPath, {
+        params: {
+          id: user.id,
+        },
+      });
+  },
+  verifyUser(data) {
+    return axios
+      .post('http://localhost:8080/authentication', data);
   },
 };
