e4ab5b8d1683a1084ab1fbe01241df21aff6e121
icestormikk <jigalow.pavel2015@yandex.ru>
2022-12-01 19:13:52 +0300

    added elements to manage the user's state

--- aggregator-front/src/components/Header.jsx
+++ aggregator-front/src/components/Header.jsx
@@ -2,17 +2,25 @@ import '../css/index.css';
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { AiOutlineClose } from 'react-icons/ai';
+import {
+  AiFillCaretDown, AiFillSetting, AiOutlineClose,
+} from 'react-icons/ai';
 import { BsFillPersonFill } from 'react-icons/bs';
+import { BiExit } from 'react-icons/bi';
+import { RiAdminLine } from 'react-icons/ri';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeHeader, openHeader } from '../store/HeaderSlice';
 import Logo from './LogoComponent';
+import { logout } from '../store/UserInfoSlice';
+import EditUserModal from './special/forms/EditUserModal';
 
 export default function Header() {
   const dispatch = useDispatch();
   const navigator = useNavigate();
   const currentUser = useSelector((state) => state.users);
-  const isOpen = useSelector((state) => state.header.isOpen);
+  const isListOpen = useSelector((state) => state.header.isOpen);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [isUserActionsListOpen, setIsUserActionsListOpen] = React.useState(false);
   const links = [
     { name: 'Услуги', url: '/services' },
     { name: 'О нас', url: '/about' },
@@ -32,7 +40,7 @@ export default function Header() {
         >
           <Logo textSizeProperty="text-3xl" />
         </Link>
-        {isOpen ? (
+        {isListOpen ? (
           <AiOutlineClose
             onClick={() => dispatch(closeHeader())}
             className="navbar-buttons"
@@ -43,10 +51,10 @@ export default function Header() {
             className="navbar-buttons"
           />
         )}
-        <div className={`absolute ${isOpen ? 'z-20' : 'z-2'} left-0 xl:static w-full xl:w-max`}>
+        <div className={`absolute ${isListOpen ? 'z-20' : 'z-2'} left-0 xl:static w-full xl:w-max`}>
           <ul
             className={
-              `text-2xl xl:flex xl:items-center bg-gray-100 xl:gap-10 xl:static xl:w-auto xl:px-0 xl:pb-0 text-secondary-color px-9 pb-4 transition-all duration-75 ease-in ${isOpen
+              `text-2xl xl:flex xl:items-center bg-gray-100 xl:gap-10 xl:static xl:w-auto xl:px-0 xl:pb-0 text-secondary-color px-9 pb-4 transition-all duration-75 ease-in ${isListOpen
                 ? 'top-20'
                 : 'top-[-490px] hidden'}`
             }
@@ -68,7 +76,54 @@ export default function Header() {
             <li>
               {
                 currentUser.isLoggedIn ? (
-                  <span>{`Привет, ${currentUser.userInfo.username}`}</span>
+                  <>
+                    <EditUserModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+                    <div className="text-base">
+                      <p>Вы вошли как:</p>
+                      <div className="flex flex-row gap-2 justify-between items-center relative">
+                        <span>{`${currentUser.userInfo.firstname} ${currentUser.userInfo.lastname}`}</span>
+                        <button type="button" onClick={() => setIsUserActionsListOpen((prevState) => !prevState)}>
+                          <AiFillCaretDown />
+                        </button>
+                        <div className={`absolute bg-gray-200 xl:w-max w-full top-full right-0 text-left rounded-b-sm ${isUserActionsListOpen ? 'flex flex-col z-[1]' : 'hidden'}`}>
+                          {
+                            currentUser.userInfo.roles.includes('admin') && (
+                              <button
+                                type="button"
+                                className="break-words p-2 flex flex-row justify-between items-center gap-2 border-b-[1px] border-b-gray-300 hover:bg-gray-300"
+                                onClick={() => {
+                                  navigator('/administration');
+                                  setIsUserActionsListOpen(false);
+                                }}
+                              >
+                                Панель администратора
+                                <RiAdminLine className="text-xl" />
+                              </button>
+                            )
+                          }
+                          <button
+                            type="button"
+                            className="border-b-[1px] border-b-gray-300 flex flex-row justify-between items-center p-2 gap-2 hover:bg-gray-300"
+                            onClick={() => setIsModalOpen(true)}
+                          >
+                            Управление аккаунтом
+                            <AiFillSetting className="text-xl" />
+                          </button>
+                          <button
+                            type="button"
+                            className="w-full h-full p-2 text-left flex flex-row justify-between items-center hover:bg-gray-300"
+                            onClick={() => {
+                              dispatch(logout());
+                              setIsUserActionsListOpen(false);
+                            }}
+                          >
+                            <span>Выйти</span>
+                            <BiExit className="text-xl" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </>
                 ) : (
                   <button
                     type="button"
